d145e516ac227f4a690c67cec868d3404af5a249
Divyanshu <divyanshu.r46956@gmail.com>
2019-08-02 13:51:37 +0200

    Code cleanup

--- application-menu-demo/src/main.js
+++ application-menu-demo/src/main.js
@@ -1,44 +1,37 @@
 
 
 const electron = require("electron");
-
 const app = electron.app;
-
 const Menu = electron.Menu;
-
 const BrowserWindow = electron.BrowserWindow;
 
-app.on("ready",_=>{
-
+app.on("ready", _ => {
 	new BrowserWindow();
-
 	const name = electron.app.getName();
-
 	const template = [{
 
 		label: name,
-		submenu:[{
+		submenu: [{
 
 			label: `About ${name}`,
-			click: _=>{
+			click: _ => {
 				console.log('Clicked !');
 			},
-			role :'about'
-		},{
-				type: 'separator'
+			role: 'about'
+		}, {
+			type: 'separator'
 		},
 		{
-				label:"Quit",
-				click:_=>{
+			label: "Quit",
+			click: _ => {
 
-					app.quit();
-				},
-				accelerator: 'Cmd + Q'
+				app.quit();
+			},
+			accelerator: 'Cmd + Q'
 		}]
 	}]
 
 	// api for menu.
 	const menu = Menu.buildFromTemplate(template);
-
 	Menu.setApplicationMenu(menu);
 })
--- clipboard-electron-demo/src/main.js
+++ clipboard-electron-demo/src/main.js
@@ -1,96 +1,70 @@
-
 const electron = require("electron");
 const path = require("path");
-const {app,Tray,Menu,clipboard,globalShortcut} = electron;
+const {
+  app,
+  Tray,
+  Menu,
+  clipboard,
+  globalShortcut
+} = electron;
 
 const STACK_SIZE = 5;
 const MAX_LENGTH = 20;
 
-function addToStack(item,stack){
-
-	return [item].concat(stack.length >= STACK_SIZE ? stack.slice(0,stack.length - 1) : stack)
-
-}
-
-function formatItem(item){
-
-	return item.length > MAX_LENGTH ? item.substr(0,MAX_LENGTH) + '...' : item;
+function addToStack(item, stack) {
+  return [item].concat(stack.length >= STACK_SIZE ? stack.slice(0, stack.length - 1) : stack)
 
 }
 
-function registerShortcuts(globalShortcut,clipboard,stack){
+function formatItem(item) {
+  return item.length > MAX_LENGTH ? item.substr(0, MAX_LENGTH) + '...' : item;
+}
 
-
+function templateForStack(clipboard, stack) {
+  return stack.map((item, i) => {
+    return {
+      label: `copy : ${formatItem(item)}`,
+      click: _ => clipboard.writeText(item)
+    }
+  })
 
 }
 
-function templateForStack(clipboard,stack){
+function checkClipboardForChange(clipboard, onChange) {
 
-	return stack.map((item,i) =>{
+  let cached = clipboard.readText();
+  let latest;
 
-		return{
-			label : `copy : ${formatItem(item)}`,
-			click : _=> clipboard.writeText(item)
-		}
-
-	})
-
-}
-
-
-
-
-function checkClipboardForChange(clipboard,onChange){
-
-	let cached = clipboard.readText();
-
-	let latest;
-
-	setInterval(_=>{
-
-		latest = clipboard.readText();
-
-		if(latest != cached){
-
-			cached = latest;
-
-			onChange(cached);
-		}
-
-	},1000)
+  setInterval(_ => {
+    latest = clipboard.readText();
+    if (latest != cached) {
+      cached = latest;
+      onChange(cached);
+    }
+  }, 1000)
 
 }
 
+app.on("ready", _ => {
 
-app.on("ready",_=>{
+  const tray = new Tray(path.join("src", "marker.png"));
+  let stack = [];
+  tray.setContextMenu(Menu.buildFromTemplate([
 
-	const tray = new Tray(path.join("src","marker.png"));
-
-	let stack = [];
-
-	tray.setContextMenu(Menu.buildFromTemplate([
-
-	{
-		label : '<Empty>',
-		enabled : false
+    {
+      label: '<Empty>',
+      enabled: false
 	}
+	
+  ]));
 
-	]));
+  checkClipboardForChange(clipboard, text => {
 
+    stack = addToStack(text, stack);
+    tray.setContextMenu(Menu.buildFromTemplate(templateForStack(clipboard, stack)));
+    console.log(templateForStack(clipboard, stack));
+    registerShortcuts(globalShortcut, clipboard, stack);
 
-
-	checkClipboardForChange(clipboard,text =>{
-
-		stack = addToStack(text,stack);
-
-		tray.setContextMenu(Menu.buildFromTemplate(templateForStack(clipboard,stack)));
-
-		console.log(templateForStack(clipboard,stack));
-
-		registerShortcuts(globalShortcut,clipboard,stack);
-
-	});
-
-
+  });
 
 })
